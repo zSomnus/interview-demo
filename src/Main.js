678252263a0508cbb4628bc373b4338d897a08c9
Ella Zhang <ellazhang37@gmail.com>
2021-09-19 22:25:54 -0300

    Add duplication and deletion for cards

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MyCard from './MyCard';
 import MyDrawer from './MyDrawer';
 
@@ -9,13 +9,43 @@ const Main = () => {
     setVisible(true);
   };
 
+  let cardContent = {
+    title: 'Custom Title',
+    body: 'Custom body content',
+    titleColor: '#000000',
+    bodyColor: '#000000',
+    panelColor: '#ffffff',
+    panelRadius: 10,
+  };
+
+  let cards = [];
+
+  const [cardMap, setCardMap] = useState([]);
+
+  const updateCards = () => {
+    let index = 0;
+    console.log(`card length: ${cards.length}`);
+    let isDeletionDisabled = cards.length < 2;
+    let newCardMap = cards.map(() => (
+      <div key={index}>
+        {MyCard(index++, cards, showDrawer, updateCards, isDeletionDisabled)}
+      </div>
+    ));
+    setCardMap([...newCardMap]);
+  };
+
+  useEffect(() => {
+    cards.push(cardContent);
+    updateCards();
+  }, []);
+
   const onClose = () => {
     setVisible(false);
   };
 
   return (
     <div>
-      {MyCard(showDrawer)}
+      {cardMap}
       {MyDrawer(onClose, visible)}
     </div>
   );
--- src/MyCard.js
+++ src/MyCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Card, Divider, Button, Row, Col } from 'antd';
-import { EditTwoTone, CopyTwoTone, DeleteTwoTone } from '@ant-design/icons';
-const CardTitle = (showDrawer) => {
+import {
+  EditTwoTone,
+  CopyTwoTone,
+  DeleteTwoTone,
+  getTwoToneColor,
+} from '@ant-design/icons';
+
+const CardTitle = (
+  titleText,
+  titleColor,
+  showDrawer,
+  duplicateCard,
+  deleteCard,
+  isDeletionIconDisabled
+) => {
   return (
     <div className="card-title" style={{ display: 'flex' }}>
       <Row wrap={false} align="middle">
         <Col flex={2}>
-          <h1>Custom Title</h1>
+          <h1 style={{ color: titleColor }}>{titleText}</h1>
         </Col>
         <Col flex={20}></Col>
         <Col style={{ textAlign: 'right' }} flex={2}>
@@ -15,22 +28,68 @@ const CardTitle = (showDrawer) => {
             icon={<EditTwoTone />}
             onClick={showDrawer}
           ></Button>
-          <Button type="link" icon={<CopyTwoTone />}></Button>
-          <Button type="link" disabled icon={<DeleteTwoTone />}></Button>
+          <Button
+            type="link"
+            icon={<CopyTwoTone />}
+            onClick={duplicateCard}
+          ></Button>
+          <Button
+            type="link"
+            icon={<DeleteTwoTone style={{ color: { getTwoToneColor } }} />}
+            disabled={isDeletionIconDisabled}
+            onClick={deleteCard}
+          ></Button>
         </Col>
       </Row>
     </div>
   );
 };
 
-const MyCard = (showDrawer) => {
+const MyCard = (
+  index,
+  cards,
+  showDrawer,
+  updateCards,
+  isDeletionIconDisabled
+) => {
+  const duplicateCard = () => {
+    cards.splice(index, 0, cards[index]);
+    updateCards();
+    console.log(cards);
+    console.log(`Duplicate from index: ${index}`);
+  };
+
+  const deleteCard = () => {
+    cards.splice(index, 1);
+    updateCards();
+    console.log(`Rrmove from index: ${index}`);
+  };
+
   return (
     <Row justify="center">
       <Col span={20}>
-        <Card className="cards" bodyStyle={{ paddingTop: 0, paddingBottom: 0 }}>
-          {CardTitle(showDrawer)}
+        <Card
+          className="cards"
+          bodyStyle={{
+            paddingTop: 0,
+            paddingBottom: 0,
+          }}
+          style={{
+            borderRadius: cards[index].panelRadius,
+            backgroundColor: cards[index].panelColor,
+          }}
+        >
+          {CardTitle(
+            cards[index].title,
+            cards[index].titleColor,
+            showDrawer,
+            duplicateCard,
+            deleteCard,
+            isDeletionIconDisabled
+          )}
           <Divider style={{ margin: 0 }} />
-          <p>Custom body text</p>
+          <p style={{ color: cards[index].bodyColor }}>{cards[index].body}</p>
+          {`${isDeletionIconDisabled}`}
         </Card>
       </Col>
     </Row>
